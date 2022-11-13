4476ebb73796f097682535d946c525b75ed0af33
ducnt <ducconit@gmail.com>
2022-11-13 20:46:49 +0700

    refactor

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,17 +1,19 @@
-function Box() {
-  return (
-    <div className="box">
-      <div className="top" style={{'backgroundImage': "url('/images/top.jpg')"}}></div>
-      <div>
-        {[...Array(4)].map((v, i) => (
-          <span key={i} style={{'--i': i}}>
-            <img src={'/images/' + i + '.jpg'} alt="" />
-          </span>
-        ))}
-      </div>
-      <div className="bottom" style={{'backgroundImage': "url('/images/bottom.jpg')"}}></div>
-    </div>
-  );
-}
+import React from "react";
 
-export default Box;
+export default class Box extends React.Component{
+  render() {
+    return (
+      <div className="box">
+        <div className="top" style={{backgroundImage: "url('/images/top.jpg')"}}></div>
+        <div>
+          {[...Array(4)].map((v, i) => (
+            <span key={i} style={{'--i': i}}>
+              <img src={'/images/' + i + '.jpg'} alt="" />
+            </span>
+          ))}
+        </div>
+        <div className="bottom" style={{backgroundImage: "url('/images/bottom.jpg')"}}></div>
+      </div>
+    );
+  }
+}
--- src/components/MP3.js
+++ src/components/MP3.js
@@ -1,9 +1,16 @@
-function MP3(props){
-    let autoplay=props.autoPlay??true;
-    let autoReplay=props.autoReplay??true;
-    return (
-        <audio src={props.link} autoPlay={autoplay} loop={autoReplay}></audio>
-    )
-}
+import React from 'react';
 
-export default MP3
+export default class MP3 extends React.Component {
+  autoPlay;
+  autoReplay;
+  link;
+  constructor(props) {
+    super(props);
+    this.autoPlay = props.autoPlay;
+    this.autoReplay = props.autoReplay;
+    this.link = props.link;
+  }
+  render() {
+    return <audio src={this.link} autoPlay={this.autoPlay} loop={this.autoReplay}></audio>;
+  }
+}
