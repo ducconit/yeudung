19f134e136d1bf9251f93b187c20c992459ec7d0
ducnt <ducconit@gmail.com>
2022-11-13 22:54:50 +0700

    add heart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Box from './components/Box';
+import Heart from './components/Heart';
 import MP3 from './components/MP3';
 let linkMP3 = '/anh-yeu-em-cuc.mp3';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div>
       <MP3 link={linkMP3} />
-      <Box />
+      {/* <Box /> */}
+      <Heart width={'auto'} height={'auto'} />
     </div>
   );
 }
