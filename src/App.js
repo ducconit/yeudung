dbda7d3aab1d820af715c7ce442995dd4ffa73e2
ducnt <ducconit@gmail.com>
2022-11-19 17:33:11 +0700

    add effect typing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Box from './components/Box';
 import Heart from './components/Heart';
 import MP3 from './components/MP3';
+import Typo from './components/Typo'
+
 let linkMP3 = '/anh-yeu-em-cuc.mp3';
 
 function App() {
   return (
     <>
       <MP3 link={linkMP3} />
-      <h2 className="typing">I LOVE YOU PẶC PẶC...</h2>
+      <Typo />
 
       <div className="container">
         <Box />
